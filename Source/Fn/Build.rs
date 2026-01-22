77bad79749bfa827a0b0983247150d83696a36fc
Nikola Hristov <Nikola@PlayForm.Cloud>
2026-01-22 18:57:06 +0200

    Rename Entry to _Entry to suppress unused variable warning

--- Source/Fn/Build.rs
+++ Source/Fn/Build.rs
@@ -1,6 +1,6 @@
 use dashmap::DashMap;
 
-pub async fn Fn(Entry:&str) -> Result<DashMap<u64, (String, String)>, Box<dyn std::error::Error>> {
+pub async fn Fn(_Entry:&str) -> Result<DashMap<u64, (String, String)>, Box<dyn std::error::Error>> {
 	let Build = DashMap::new();
 
 	Ok(Build)
