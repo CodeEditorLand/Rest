592b6ceb4c15d241f7fbabcbdceee1ff28058b3b
Nikola Hristov <Nikola@PlayForm.Cloud>
2026-01-22 18:42:58 +0200

    Add DashMap and Group module to Build.rs

--- Source/Fn/Build.rs
+++ Source/Fn/Build.rs
@@ -1,5 +1,13 @@
+use dashmap::DashMap;
+
 pub async fn Fn(Entry:&str) -> Result<DashMap<u64, (String, String)>, Box<dyn std::error::Error>> {
 	let Build = DashMap::new();
 
 	Ok(Build)
+}
+
+pub mod Group {
+	pub fn Fn(output: Vec<(String, String)>) {
+		println!("Processed {} files", output.len());
+	}
 }
